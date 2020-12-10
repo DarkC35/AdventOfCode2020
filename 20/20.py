84105dd862370ecc2ebf6b9ce215b496b6f6018a
Kevin Hannes Haunschmied <khaunschmied.mmt-m2019@fh-salzburg.ac.at>
2020-12-10 20:49:34 +0100

    format code of 20.py

--- 20/20.py
+++ 20/20.py
@@ -3,19 +3,23 @@ from timeit import default_timer as timer
 
 # not efficient (takes some hours)
 def count_arrangements_not_efficient(adapters, current_jolts):
-    compatible_adapter_indexes = [i for i, x in enumerate(adapters[0:3]) if x <= current_jolts + 3]
-    if len(compatible_adapter_indexes) ==  0:
+    compatible_adapter_indexes = [
+        i for i, x in enumerate(adapters[0:3]) if x <= current_jolts + 3
+    ]
+    if len(compatible_adapter_indexes) == 0:
         return 1
     count = 0
     for index in compatible_adapter_indexes:
-        count += count_arrangements_not_efficient(adapters[index+1:], adapters[index])
+        count += count_arrangements_not_efficient(
+            adapters[index+1:], adapters[index])
     return count
 
 
 # solution with caching! (thx Dani & Ginzi)
 def count_arrangements(adapters, current_jolts, cache):
-    compatible_adapter_indexes = [i for i, x in enumerate(adapters[0:3]) if x <= current_jolts + 3]
-    if len(compatible_adapter_indexes) ==  0:
+    compatible_adapter_indexes = [i for i, x in enumerate(
+        adapters[0:3]) if x <= current_jolts + 3]
+    if len(compatible_adapter_indexes) == 0:
         return 1
     cached = cache.get(current_jolts)
     if cached:
